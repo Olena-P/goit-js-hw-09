2b36e3df64e7afa420e5b5f52f77bec309a55572
Olena-P <73667823+Olena-P@users.noreply.github.com>
2023-09-23 18:03:41 +0000

    Deploying to gh-pages from @ Olena-P/goit-js-hw-09@28f305230e786643c9a6669d48d4a7493dd006ba 🚀

---
+++ partials/01-color-switcher.f6a14b0a.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("startButton"),e=document.getElementById("stopButton"),d=document.body;let n;t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,n=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.f6a14b0a.js.map
