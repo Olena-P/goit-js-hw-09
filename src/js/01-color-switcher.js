28f305230e786643c9a6669d48d4a7493dd006ba
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2023-09-23 21:02:51 +0300

    Added color switcer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const body = document.body;
+let intervalId;
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(intervalId);
+});
